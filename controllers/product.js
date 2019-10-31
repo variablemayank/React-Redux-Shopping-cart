aaaac4fb8588846756e11e0751d8e6aae8edd3bf
mayank_20 <mayank202020@rediffmail.com>
2019-10-31 23:00:53 +0530

    complete route for the product

--- controllers/product.js
+++ controllers/product.js
@@ -24,13 +24,42 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
+
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+        } = fields;
+
+        if(
+            !name ||
+            !description || 
+            !price ||
+            !category || 
+            !quantity 
+
+        ) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+        console.log("fields",fields);
+
         let product = new Product(fields);
-        console.log(files)
+
+        console.log("product",product)
         if (files.photo) {
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1 mb in size"
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
-        console.log(product)
+      
         product.save((err, result) => {
             if (err) {
                 console.log("asdas")
@@ -62,3 +91,61 @@ exports.remove = (req,res) => {
         });
     });
 }
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            });
+        }
+
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping,
+        } = fields;
+
+        if(
+            !name ||
+            !description || 
+            !price ||
+            !category || 
+            !quantity || 
+            !shipping 
+
+        ) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+        let product = req.product;
+        product = _.extend(product,fields);
+        console.log(files)
+        if (files.photo) {
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1 mb in size"
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+        console.log(product)
+        product.save((err, result) => {
+            if (err) {
+                console.log("asdas")
+                console.log(result)
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+};
